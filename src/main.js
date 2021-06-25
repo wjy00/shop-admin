a44dc50be4e7911f41bfb7b447790552e8a4654e
wjy00 <842849788@qq.com>
2021-06-25 16:47:57 +0800

    商品功能

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
+// 导入表格树
 import TreeTable from 'vue-table-with-tree-grid'
-
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 // 引入全局样式
 import './assets/css/global.css'
@@ -24,7 +30,25 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.use(ElementUI)
+// 组件全局注册 表格树
 Vue.component('tree-table', TreeTable)
+// 全局注册富文本编辑器
+Vue.use(VueQuillEditor)
+
+// 全局时间过滤器
+Vue.filter('dataFormat', function (originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  // yyyy-mm-dd hh:mm:ss
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
